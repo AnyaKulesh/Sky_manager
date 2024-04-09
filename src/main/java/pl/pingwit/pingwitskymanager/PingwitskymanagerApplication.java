deb7873a0b831ee6870b22860a443c42e09fe9a0
AnyaKulesh <annakuleshV94@gmail.com>
2024-04-10 00:50:18 +0300

    Final project

---
+++ src/main/java/pl/pingwit/pingwitskymanager/PingwitskymanagerApplication.java
@@ -0,0 +1,13 @@
+package pl.pingwit.pingwitskymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PingwitskymanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PingwitskymanagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/pingwit/pingwitskymanager/PingwitskymanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.pingwit.pingwitskymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PingwitskymanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
